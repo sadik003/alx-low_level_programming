32da5b93cf22f986a5f5e4d206df6ad1a6a5a1de
abubakar <abubakarunwaha@gmail.com>
2022-09-09 19:19:56 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,51 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
 
+
+
 /**
+
  * main - Prints a random number and states whether
+
  *        it is positive, negative, or zero.
+
  *
+
  * Return: Always 0.
+
 */
+
 int main(void)
+
 {
-0-positive_or_negative.c int n;
 
-0-positive_or_negative.c srand(time(0));
-0-positive_or_negative.c n = rand() - RAND_MAX / 2;
+README.md int n;
 
-0-positive_or_negative.c if (n > 0)
-0-positive_or_negative.c 0-positive_or_negative.c printf("%d is positive\n", n);
-0-positive_or_negative.c else if (n < 0)
-0-positive_or_negative.c 0-positive_or_negative.c printf("%d is negative\n", n);
-0-positive_or_negative.c else
-0-positive_or_negative.c 0-positive_or_negative.c printf("%d is zero\n", n);
 
-0-positive_or_negative.c return (0);
+
+README.md srand(time(0));
+
+README.md n = rand() - RAND_MAX / 2;
+
+
+
+README.md if (n > 0)
+
+README.md README.md printf("%d is positive\n", n);
+
+README.md else if (n < 0)
+
+README.md README.md printf("%d is negative\n", n);
+
+README.md else
+
+README.md README.md printf("%d is zero\n", n);
+
+
+
+README.md return (0);
+
 }
-
