ae264f58c4b086cc44bf0205ff689730e5e8bd36
abubakar <abubakarunwaha@gmail.com>
2022-09-11 10:34:33 -0700

    Print string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * title: a program to print string using printf
+ * author: abubakar unwaha
+ */
+#include <stdio>
+
+/**
+ * main - the main function print string
+ *
+ * Return: return zero
+ */
+
+	int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,%d\n");
+	return (0);
+
+}
