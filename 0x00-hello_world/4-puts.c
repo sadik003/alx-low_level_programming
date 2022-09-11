0eb8af5447a29750c59fca7375c2321de6cd2519
abubakar <abubakarunwaha@gmail.com>
2022-09-11 09:13:31 -0700

    Program to print string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-/**
- *
+/*
  * Title: A program that prints strings
  * Author: Abubakar Unwaha
  */
 #include <stdio.h>
-	/*
- 	 *A program to output string using puts
- 	 *followed by a new line 
- 	 */
-int main(void) 
+/* This function outputs string */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
