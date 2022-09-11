88d34b01ce920368dd876b9a3a93b34bbd8bbbe9
abubakar <abubakarunwaha@gmail.com>
2022-09-11 07:54:51 -0700

    To print string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/*
+ * Title: A program that prints strings
+ * Author: Abubakar Unwaha
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
