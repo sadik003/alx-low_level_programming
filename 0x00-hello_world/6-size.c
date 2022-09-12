8b8779282dbe9964cbc6f1c3efc4bdfa81583834
abubakar <abubakarunwaha@gmail.com>
2022-09-11 20:43:58 -0700

    print sizes of Type in computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+/* title: This program print the size of type in computer
+ * 
+ * author: Abubakar Unwaha
+ *
+ */
+#include<stdio.h>
+
+/**
+ * main - This function prints the size of type in computer system
+ *
+ * Return: return zero
+ */
+	int main(void)
+
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+
+}
