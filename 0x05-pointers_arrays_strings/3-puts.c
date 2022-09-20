d68bce3634a685aed05bc7efa544220cb4509290
abubakar <abubakarunwaha@gmail.com>
2022-09-19 21:09:22 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * _put - prints string followed by a new line
+ * _puts - prints string followed by a new line
  *
  * @str: char string
  *
@@ -12,7 +13,7 @@ void _puts(char *str)
 	int c;
 
 	for (c = 0; str[c] != '\0'; c++)
-{	
+{
 	_putchar(str[c]);
 }
 	_putchar('\n');
