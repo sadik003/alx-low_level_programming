a0c1200f92851d4461d513b7c433ec67f68a1436
abubakar <abubakarunwaha@gmail.com>
2022-09-19 20:39:33 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _put - prints string followed by a new line
+ *
+ * @str: char string
+ *
+ * return: 0
+ */
+
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0' c++)
+{	
+	_putchar(str(c));
+}
+	_putchar('\n');
+}
