316e3cc6b50810a548b5b9a84de7d70ef4288352
abubakar <abubakarunwaha@gmail.com>
2022-09-22 20:34:00 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,19 @@
  *
  * Description: prints _putchar using putchar prototype
  *
- * Return: Always 0 (Success)
-*/
-
+ * Return: Always 0
+ *
+ */
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
