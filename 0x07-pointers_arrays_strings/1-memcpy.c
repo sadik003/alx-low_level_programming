c57345a5fd419a52c3224bc277fff27a34ff4a7c
abubakar <abubakarunwaha@gmail.com>
2022-09-26 18:17:06 -0700

    copy from one memory area to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function that copies memory area
+ *
+ * @dest: destination memory area
+ * @scr: memory address to copy from
+ * @n: data to be copied
+ *
+ * Return:dest
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = *src;
+	}
+	return (dest);
+}
