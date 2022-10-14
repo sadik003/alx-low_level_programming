96cc482713ddf83ebb1e51ebb7fc72716845e2b9
abubakar <abubakarunwaha@gmail.com>
2022-10-13 18:57:31 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+/*
+ * 
+ * Title: a function that check if an alphabet is lowercase 
+ * Author: Abubakar Unwaha
+ *
+ */
+
+#include "main.h"
+
+/*
+ * _islower - a function that check weather and alphabet is lower or otherwise
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if lowercase and 0 if otherwise
+ *
+ */
+	int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
