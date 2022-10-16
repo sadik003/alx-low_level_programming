958924d8fb1669cbd53f4612aa6b6d8bb590739f
abubakar <abubakarunwaha@gmail.com>
2022-10-15 18:46:17 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -24,7 +24,13 @@ int _isalpha(int c)
 	int ch;
 
 	if (ch >= 48 && ch <= 57)
+	{
+		return (1);
+	}
 	else if (ch >= 65 && ch <= 90)
+	{
+		return (1);
+	}
 	else if (ch >= 97 && ch <= 122)
 			{
 				return (1);
