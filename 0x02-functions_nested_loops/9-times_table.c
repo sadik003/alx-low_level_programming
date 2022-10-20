bf420fc0c58bdeef70823dcfe0821130f88323c5
abubakar <abubakarunwaha@gmail.com>
2022-10-19 20:57:00 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,10 +18,11 @@ void times_table(void)
 			k = i * j;
 			if (j == 0)
 			{
-			_putchar(k + '0');
+			_putchar(k + '0')
 			}
 			if (k < 10 && j != 0)
 			{
+				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
@@ -34,5 +35,6 @@ void times_table(void)
 					_putchar((k % 10) + '0');
 				}
 		}
+		_putchar('\n');
 	}
 }
