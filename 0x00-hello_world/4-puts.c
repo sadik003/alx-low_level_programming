9971f85e813e6d98d5ff0e7b733afe611883be3f
Abubakar Unwaha <abubakarunwaha@gmail.com>
2023-05-19 05:44:16 -0700

    Puts funtion

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Abubakar Unwaha
+ * Project: Print using the puts function
+ */
+
+#include <stdio.h>
+
+/**
+ * main - this function print with puts function
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
