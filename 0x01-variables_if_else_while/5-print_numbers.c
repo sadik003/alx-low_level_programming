f795af5e815752b7e130acfb191ce083bee8f6f0
Abubakar Unwaha <abubakarunwaha@gmail.com>
2023-05-19 19:36:23 -0700

    print digit 1 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - functions that prints 0 to 9
+ * Return:0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
