55992bbc45644d276a9454b778629bc9da8a17d7
Abubakar Unwaha <abubakarunwaha@gmail.com>
2023-05-19 19:22:21 -0700

    lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - the function print lower case followed by uppercaser character
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+return (0);
+}
