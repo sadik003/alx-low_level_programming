d1d1d7d97b785c4d3530d4b8e5fe41259304b3f2
Abubakar Unwaha <abubakarunwaha@gmail.com>
2023-05-23 21:52:06 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - the function check if character is lowercase
+ * @c: character to be checked
+ * Return: 1 for success and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z')
+}
