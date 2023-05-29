f39d085e364444d4bb5e6c5e1ace05e43cce8d2f
Abubakar Unwaha <abubakarunwaha@gmail.com>
2023-05-29 03:33:06 -0700

    reset to 98

--- 0x04-more_functions_nested_loops/0x05-pointers_arrays_strings/main.h
+++ 0x04-more_functions_nested_loops/0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,4 +15,3 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 #endif
-
